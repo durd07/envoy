a7070180edda65bfdfc67a8c95e7d5685dc78d97
Felix Du <felix.du@nokia-sbell.com>
2020-08-31 22:51:39 +0800

    add sip proxy well known names

--- source/extensions/filters/network/well_known_names.h
+++ source/extensions/filters/network/well_known_names.h
@@ -48,6 +48,8 @@ public:
   const std::string ZooKeeperProxy = "envoy.filters.network.zookeeper_proxy";
   // WebAssembly filter
   const std::string Wasm = "envoy.filters.network.wasm";
+  // SIP proxy filter
+  const std::string SipProxy = "envoy.filters.network.sip_proxy";
 };
 
 using NetworkFilterNames = ConstSingleton<NetworkFilterNameValues>;
