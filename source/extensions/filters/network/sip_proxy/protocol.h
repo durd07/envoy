2a123b304b461cad065d6fe23b7960a4764414ff
Felix Du <felix.du@nokia-sbell.com>
2021-06-10 09:23:31 +0800

    forward sip-proxy extensions
    Signed-off-by: Felix Du <felix.du@nokia-sbell.com>

---
+++ source/extensions/filters/network/sip_proxy/protocol.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "envoy/buffer/buffer.h"
+
+#include "extensions/filters/network/sip_proxy/conn_state.h"
+#include "extensions/filters/network/sip_proxy/metadata.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace NetworkFilters {
+namespace SipProxy {
+
+class SipSettings {
+public:
+  SipSettings(std::chrono::milliseconds transaction_timeout)
+      : transaction_timeout_(transaction_timeout) {}
+  std::chrono::milliseconds transactionTimeout() { return transaction_timeout_; }
+
+private:
+  std::chrono::milliseconds transaction_timeout_;
+};
+
+/**
+ * A DirectResponse manipulates a Protocol to directly create a Sip response message.
+ */
+class DirectResponse {
+public:
+  virtual ~DirectResponse() = default;
+
+  enum class ResponseType {
+    // DirectResponse encodes MessageType::Reply with success payload
+    SuccessReply,
+
+    // DirectResponse encodes MessageType::Reply with an exception payload
+    ErrorReply,
+
+    // DirectResponse encodes MessageType::Exception
+    Exception,
+  };
+
+  /**
+   * Encodes the response via the given Protocol.
+   * @param metadata the MessageMetadata for the request that generated this response
+   * @param proto the Protocol to be used for message encoding
+   * @param buffer the Buffer into which the message should be encoded
+   * @return ResponseType indicating whether the message is a successful or error reply or an
+   *         exception
+   */
+  virtual ResponseType encode(MessageMetadata& metadata, Buffer::Instance& buffer) const PURE;
+};
+} // namespace SipProxy
+} // namespace NetworkFilters
+} // namespace Extensions
+} // namespace Envoy
