58c5d1304fea458cd7e094b6fc655e84aab51286
Felix Du <felix.du@nokia-sbell.com>
2021-08-23 02:08:09 +0000

    update

--- source/extensions/filters/network/sip_proxy/decoder.h
+++ source/extensions/filters/network/sip_proxy/decoder.h
@@ -114,8 +114,7 @@ public:
    * @return DecoderEventHandler& a new DecoderEventHandler for a message.
    */
   virtual DecoderEventHandler& newDecoderEventHandler(MessageMetadataSharedPtr metadata) PURE;
-  //virtual absl::string_view getLocalIp() PURE;
-  virtual void getLocalIp() PURE;
+  virtual absl::string_view getLocalIp() PURE;
 };
 
 /**
@@ -197,134 +196,22 @@ private:
         absl::flat_hash_map<HeaderType,
                             std::function<int(HeaderHandler*, absl::string_view& header)>>;
 
-    virtual int processVia(absl::string_view& header) {
-      if (!isFirstVia()) {
-        return 0;
-      }
-
-      metadata()->setTransactionId(header);
-      setFirstVia(false);
-
-      return 0;
-    }
-
-    virtual int processPath(absl::string_view& header) {
-      if (header.find(";ep=") != absl::string_view::npos) {
-        // Already has ep in Path
-        return 0;
-      }
-      auto pos = header.find(">");
-      if (pos == absl::string_view::npos) {
-        // No url
-        return 0;
-      }
-      metadata()->setOperation(
-          Operation(OperationType::Insert, rawOffset() + pos, InsertOperationValue(";ep=")));
-      return 0;
-    };
-
+    virtual int processVia(absl::string_view& header);
+    virtual int processContact(absl::string_view& header);
+    virtual int processPath(absl::string_view& header);
     virtual int processEvent(absl::string_view& header) {
       UNREFERENCED_PARAMETER(header);
       return 0;
     };
-
-    virtual int processRoute(absl::string_view& header) {
-      if (!isFirstRoute()) {
-        return 0;
-      }
-      setFirstRoute(false);
-
-      if (auto loc = header.find(";ep="); loc != absl::string_view::npos) {
-        // already have ep
-        auto start = loc + 4;
-        if (auto end = header.find(">", start); end != absl::string_view::npos) {
-          Buffer::OwnedImpl buffer;
-          buffer.add(header.substr(start, end - start));
-          // metadata()->setRouteEP(Base64::decode(std::string(header.substr(start, end - start))));
-          metadata()->setRouteEP(header.substr(start, end - start));
-        }
-      }
-
-      metadata()->setTopRoute(header);
-      metadata()->setDomain(Decoder::domain(header, HeaderType::Route));
-
-      return 0;
-    }
-    virtual int processContact(absl::string_view& header) {
-      if (header.find(";ep=") != absl::string_view::npos) {
-        // Already has ep in Path
-        return 0;
-      }
-      auto pos = header.find(">");
-      if (pos == absl::string_view::npos) {
-        // No url
-        return 0;
-      }
-      metadata()->setOperation(
-          Operation(OperationType::Insert, rawOffset() + pos, InsertOperationValue(";ep=")));
-
-      if (auto pos = header.find(";inst-ip="); pos != absl::string_view::npos) {
-        metadata()->setOperation(
-            Operation(OperationType::Delete, rawOffset() + pos,
-                      DeleteOperationValue(
-                          header.substr(pos, header.find_first_of(";>", pos + 1) - pos).size())));
-        auto xsuri = header.find("sip:pcsf-cfed");
-        metadata()->setOperation(
-            Operation(OperationType::Delete, rawOffset() + xsuri, DeleteOperationValue(4)));
-      }
-
-      return 0;
-    }
+    virtual int processRoute(absl::string_view& header);
     virtual int processCseq(absl::string_view& header) {
       UNREFERENCED_PARAMETER(header);
       return 0;
     }
-    virtual int processRecordRoute(absl::string_view& header) {
-      if (header.find(";ep=") != absl::string_view::npos) {
-        // Already has ep in Path
-        return 0;
-      }
-      auto pos = header.find(">");
-      if (pos == absl::string_view::npos) {
-        // No url
-        return 0;
-      }
-      metadata()->setOperation(
-          Operation(OperationType::Insert, rawOffset() + pos, InsertOperationValue(";ep=")));
-      return 0;
-    }
-    virtual int processServiceRoute(absl::string_view& header) {
-      if (header.find(";ep=") != absl::string_view::npos) {
-        // Already has ep in Path
-        return 0;
-      }
-      auto pos = header.find(">");
-      if (pos == absl::string_view::npos) {
-        // No url
-        return 0;
-      }
-      metadata()->setOperation(
-          Operation(OperationType::Insert, rawOffset() + pos, InsertOperationValue(";ep=")));
-      return 0;
-    }
-    virtual int processWwwAuth(absl::string_view& header) {
-      if (header.find(";opaque=") != absl::string_view::npos) {
-        // already WwwAuth have opaque
-        return 0;
-      }
-
-      metadata()->setOperation(Operation(OperationType::Insert, rawOffset() + header.length(),
-                                         InsertOperationValue(";opaque=")));
-      return 0;
-    }
-
-    virtual int processAuth(absl::string_view& header) {
-      if (auto loc = header.find(";opaque="); loc != absl::string_view::npos) {
-        // already R have ep
-        metadata()->setRouteOpaque(header.substr(loc + 8));
-      }
-      return 0;
-    }
+    virtual int processRecordRoute(absl::string_view& header);
+    virtual int processServiceRoute(absl::string_view& header);
+    virtual int processWwwAuth(absl::string_view& header);
+    virtual int processAuth(absl::string_view& header);
 
     MessageMetadataSharedPtr metadata() { return parent_.metadata(); }
 
@@ -363,53 +250,33 @@ private:
   class REGISTERHeaderHandler : public HeaderHandler {
   public:
     using HeaderHandler::HeaderHandler;
-    int processVia(absl::string_view& header) override;
-    int processRoute(absl::string_view& header) override;
-    int processRecordRoute(absl::string_view& header) override;
-    int processPath(absl::string_view& header) override;
-    // int processAuth(absl::string_view& header) override;
-    // int processContact(absl::string_view& header) override;
   };
 
   class INVITEHeaderHandler : public HeaderHandler {
   public:
     using HeaderHandler::HeaderHandler;
-    int processVia(absl::string_view& header) override;
-    int processRoute(absl::string_view& header) override;
-    int processRecordRoute(absl::string_view& header) override;
   };
 
   class OK200HeaderHandler : public HeaderHandler {
   public:
     using HeaderHandler::HeaderHandler;
     int processCseq(absl::string_view& header) override;
-    int processRecordRoute(absl::string_view& header) override;
-    int processServiceRoute(absl::string_view& header) override;
-    int processContact(absl::string_view& header) override;
   };
 
   class GeneralHeaderHandler : public HeaderHandler {
   public:
     using HeaderHandler::HeaderHandler;
-    int processRoute(absl::string_view& header) override;
-    int processVia(absl::string_view& header) override;
-    int processContact(absl::string_view& header) override;
   };
 
   class SUBSCRIBEHeaderHandler : public HeaderHandler {
   public:
     using HeaderHandler::HeaderHandler;
     int processEvent(absl::string_view& header) override;
-    int processVia(absl::string_view& header) override;
-    int processRoute(absl::string_view& header) override;
-    int processContact(absl::string_view& header) override;
   };
 
   class FAILURE4XXHeaderHandler : public HeaderHandler {
   public:
     using HeaderHandler::HeaderHandler;
-    // int processWwwAuth(absl::string_view& header) PURE;
-    // int processWwwAuth(absl::string_view& header) override;
   };
 
   class REGISTERHandler : public MessageHandler {
@@ -476,11 +343,7 @@ private:
         : MessageHandler(std::make_shared<HeaderHandler>(*this), parent) {}
     ~OthersHandler() override = default;
 
-    void parseHeader(HeaderType& type, absl::string_view& header) override {
-      if (type == HeaderType::Via) {
-        handler_->processVia(header);
-      }
-    };
+    void parseHeader(HeaderType& type, absl::string_view& header) override;
   };
 
   class FAILURE4XXHandler : public MessageHandler {
