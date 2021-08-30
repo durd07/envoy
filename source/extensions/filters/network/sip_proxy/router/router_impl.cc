959b2f3ed38555bcfa5ff67d67b5641ce7ddc18a
Felix Du <felix.du@nokia-sbell.com>
2021-08-30 09:25:27 +0000

    update

--- source/extensions/filters/network/sip_proxy/router/router_impl.cc
+++ source/extensions/filters/network/sip_proxy/router/router_impl.cc
@@ -168,30 +168,27 @@ FilterStatus Router::messageBegin(MessageMetadataSharedPtr metadata) {
   auto& transaction_info = (*transaction_infos_)[cluster_name];
 
   auto message_handler_with_loadbalancer = [&]() {
-    Tcp::ConnectionPool::Instance* conn_pool;
     Upstream::HostDescriptionConstSharedPtr host;
-    for (int i = 0; i < 3; i++) {
-      // Tcp::ConnectionPool::Instance* conn_pool =
-      conn_pool = cluster->tcpConnPool(Upstream::ResourcePriority::Default, this);
-      if (!conn_pool) {
-        stats_.no_healthy_upstream_.inc();
-        callbacks_->sendLocalReply(
-            AppException(AppExceptionType::InternalError,
-                         fmt::format("no healthy upstream for '{}'", cluster_name)),
-            true);
-        return FilterStatus::StopIteration;
-      }
-
-      ENVOY_STREAM_LOG(debug, "router decoding request", *callbacks_);
-
-      // Upstream::HostDescriptionConstSharedPtr host = conn_pool->host();
-      host = conn_pool->host();
-      if (!host) {
-        return FilterStatus::StopIteration;
-      }
-      ENVOY_STREAM_LOG(debug, "DDD-1 host ip {} ", *callbacks_,
-                       host->address()->ip()->addressAsString());
+    Tcp::ConnectionPool::Instance* conn_pool =
+        cluster->tcpConnPool(Upstream::ResourcePriority::Default, this);
+    if (!conn_pool) {
+      stats_.no_healthy_upstream_.inc();
+      callbacks_->sendLocalReply(
+          AppException(AppExceptionType::InternalError,
+                       fmt::format("no healthy upstream for '{}'", cluster_name)),
+          true);
+      return FilterStatus::StopIteration;
     }
+
+    ENVOY_STREAM_LOG(debug, "router decoding request", *callbacks_);
+
+    // Upstream::HostDescriptionConstSharedPtr host = conn_pool->host();
+    host = conn_pool->host();
+    if (!host) {
+      return FilterStatus::StopIteration;
+    }
+    ENVOY_STREAM_LOG(debug, "DDD-1 host ip {} ", *callbacks_,
+                     host->address()->ip()->addressAsString());
 
     if (auto upstream_request =
             transaction_info->getUpstreamRequest(host->address()->ip()->addressAsString());
